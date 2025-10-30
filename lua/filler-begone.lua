122018c0f54519b9ee16fb0e31b96989a7682e1c
Liam Dyer <liamcdyer@gmail.com>
2025-10-30 15:41:34 -0400

    fix: handle concealed lines

--- lua/filler-begone.lua
+++ lua/filler-begone.lua
@@ -1,3 +1,23 @@
+local function should_ignore_line(buf, win, line)
+  -- line is folded
+  local fold_closed_line = vim.fn.foldclosed(line)
+  if fold_closed_line ~= -1 and fold_closed_line ~= line then return true end
+
+  -- line is concealed
+  local conceal_level = vim.wo[win].conceallevel or vim.o.conceallevel or 0
+  if conceal_level < 2 then return false end
+
+  local extmarks = vim.api.nvim_buf_get_extmarks(buf, -1, { line - 1, 0 }, { line - 1, -1 }, { details = true })
+  for _, extmark in ipairs(extmarks) do
+    if extmark[4].conceal_lines ~= nil then return true end
+  end
+
+  -- TODO: consider if line is not concealed due to cursor being on it
+  -- local conceal_cursor = vim.wo[win].concealcursor or vim.o.concealcursor or 'niv'
+
+  return false
+end
+
 --- Prevents neovim from scrolling past the bottom of the buffer and showing unnecessary `:h filler-lines`
 --- @param win number
 local function filler_begone(win)
@@ -7,57 +27,51 @@ local function filler_begone(win)
   local buf = vim.api.nvim_win_get_buf(win)
   if vim.b[buf].filler_begone == false then return end
 
-  local line_count = vim.api.nvim_buf_line_count(buf)
+  vim.api.nvim_win_call(win, function()
+    local line_count = vim.api.nvim_buf_line_count(buf)
+    local win_height = math.min(vim.api.nvim_win_get_height(win), line_count)
 
-  local win_height = math.min(vim.api.nvim_win_get_height(win), line_count)
+    -- check how many lines are visible from the buffer, starting from the line currently at the top of the window
+    local top_line = vim.fn.line('w0', win)
+    local lines_below = 0
+    for i = top_line, line_count do
+      if not should_ignore_line(buf, win, i) then
+        local line_height = vim.api.nvim_win_text_height(win, { start_row = i - 1, end_row = i - 1 }).all
+        lines_below = lines_below + line_height
+        if lines_below >= win_height then break end
+      end
+    end
 
-  -- check how many lines are visible from the buffer, starting from the line currently at the top of the window
-  local top_line = vim.fn.line('w0', win) - 1 -- 1-indexed -> 0-indexed
-  local visible_buffer_lines = vim.api.nvim_win_text_height(win, {
-    start_row = top_line,
-    end_row = math.max(math.min(top_line + win_height - 1, line_count - 1), 0),
-  }).all
-  local out_of_buf_lines = win_height - visible_buffer_lines
-
-  if out_of_buf_lines > 0 then
-    vim.api.nvim_win_call(win, function()
+    if lines_below < win_height then
       -- start from the last line, calculate the height of each line, including virtual text and wrapping
       -- until we reach the height of the window
       local accumulated_line_height = 0
-      local target_line = line_count - 1
+      local target_line = line_count
       while target_line > 0 do
-        -- ignore if line is folded
-        local fold_closed_line = vim.fn.foldclosed(target_line)
-        if fold_closed_line ~= -1 and fold_closed_line ~= target_line then
-          target_line = target_line - 1
-
-        -- otherwise, count the visible height of the line
-        else
+        if not should_ignore_line(buf, win, target_line) then
           local line_height = vim.api.nvim_win_text_height(win, {
-            start_row = target_line,
-            end_row = target_line,
+            start_row = target_line - 1,
+            end_row = target_line - 1,
           }).all
           accumulated_line_height = accumulated_line_height + line_height
 
           -- current line would go beyond win height, go back down one line
           if accumulated_line_height > win_height then target_line = target_line + 1 end
           if accumulated_line_height >= win_height then break end
-
-          target_line = target_line - 1
         end
+        target_line = target_line - 1
       end
+      target_line = math.min(target_line, line_count)
 
       -- scroll to the target line
-      vim.fn.winrestview({ topline = target_line + 1 }) -- 0-indexed -> 1-indexed
+      vim.fn.winrestview({ topline = target_line })
 
       -- force another event loop iteration, with updated topline
       -- https://github.com/neovim/neovim/issues/35633#issuecomment-3256274806
-      vim.schedule(function()
-        vim.fn.winrestview({ topline = target_line + 1 }) -- 0-indexed -> 1-indexed
-      end)
+      vim.schedule(function() vim.fn.winrestview({ topline = target_line }) end)
       vim.api.nvim_feedkeys(vim.keycode('<Ignore>'), 'ni', false)
-    end)
-  end
+    end
+  end)
 end
 
 return filler_begone
